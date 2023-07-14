ea48a124678ed9e8895e539502a143f1e75ca663
enkod-id <natanaeltamto@gmail.com>
2023-07-14 22:54:20 +0700

    this is week 05

---
+++ js/switch.js
@@ -0,0 +1,16 @@
+<script>
+      // initial state
+      if (localStorage.getItem("darkMode") === "true") {
+        document.getElementById("checkbox-dark-mode").checked = true;
+        document.body.classList.add("dark-mode");
+      }
+
+      function toggleDarkMode() {
+        document.body.classList.toggle("dark-mode");
+        if (!document.getElementById("checkbox-dark-mode").checked) {
+          localStorage.setItem("darkMode", false);
+        } else {
+          localStorage.setItem("darkMode", true);
+        }
+      }
+</script>
